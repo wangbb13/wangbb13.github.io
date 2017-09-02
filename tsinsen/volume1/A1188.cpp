31bc120dd48e275d2f70ad2254dd71c6f305bc96
wangbb <18800160527@163.com>
2017-09-02 20:39:01 +0800

    add new file

--- tsinsen/volume1/A1188.cpp
+++ tsinsen/volume1/A1188.cpp
@@ -11,27 +11,53 @@ bool cmp(pair<int, int> a, pair<int, int> b) {
 }
 
 int main() {
-  int i, j, N, R, Q;
+  int i, j, N, R, Q, r, s, p, n;
   cin >> N >> R >> Q;
-  vector< pair<int, int> > f(2*N, pair<int, int>(0, 0));
-  vector<int> c(2*N, 0);
-  for (i = 0; i < 2*N; i++) {
+  n = 2 * N;
+  vector< pair<int, int> > f(n, pair<int, int>(0, 0));
+  vector< pair<int, int> > F(n, pair<int, int>(0, 0));
+  vector<int> c(n, 0);
+  for (i = 0; i < n; i++) {
     cin >> f[i].second;
     f[i].first = i;
   }
   sort(f.begin(), f.end(), cmp);
-  for (i = 0; i < 2*N; i++) {
+  for (i = 0; i < n; i++) {
     cin >> c[i];
   }
   for (i = 0; i < R; i++) {
-    for (j = 0; j < 2 * N; j += 2) {
+    for (j = 0; j < n; j += 2) {
       if (c[f[j].first] > c[f[j+1].first]) {
         f[j].second++;
       } else {
         f[j+1].second++;
+        if (cmp(f[j+1], f[j])) {
+          pair<int, int> temp = f[j];
+          f[j] = f[j+1];
+          f[j+1] = temp;
+        }
       }
     }
-    sort(f.begin(), f.end(), cmp);
+    // sort(f.begin(), f.end(), cmp);
+    r = 0; s = 1; p = 0;
+    while (r < n && s < n) {
+      if (cmp(f[r], f[s])) {
+        F[p++] = f[r];
+        r += 2;
+      } else {
+        F[p++] = f[s];
+        s += 2;
+      }
+    }
+    while (r < n) {
+      F[p++] = f[r];
+      r += 2;
+    }
+    while (s < n) {
+      F[p++] = f[s];
+      s += 2;
+    }
+    f = F;
   }
   cout << (f[Q-1].first+1) << endl;
   return 0;
