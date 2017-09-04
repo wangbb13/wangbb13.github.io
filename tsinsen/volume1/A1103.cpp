a84cfae11ebed38074466f288058bdbc6a51da50
wangbb <18800160527@163.com>
2017-09-04 15:14:41 +0800

    modify some files

--- tsinsen/volume1/A1103.cpp
+++ tsinsen/volume1/A1103.cpp
@@ -1,54 +1,77 @@
 #include <iostream>
+#include <cstring>
 #include <cmath>
 using namespace std;
 
+// 思路要清晰再下手，简单明了为最佳
+
 int main(int argc, char const *argv[]) 
 {
-  int n, i, j, k, l, a, b;
-  int record[16][16];
-  int trace[16];
+  int n, i, j, k;
+  int record[17][17];
+  int flag[17];
   cin >> n;
   n = pow(2, n);
-  for (i = 0; i < 16; i++) {
-    for (j = 0 ; j < 16; j++) {
-      record[i][j] = 0;
-    }
-  }
-  for (i = 0; i < n; i++) {
-    record[i][i] = 1;
-  }
-  for (i = 0; i < n-1; i++) {
-    for (j = 0; j < n; j++) {
-      trace[j] = 0;
-    }
-    cout << "<" << i+1 << ">";
-    for (l = 0; l < n/2; l++) {
-      // find a
-      for (j = 0; j < n; j++) {
-        int sum = 0;
-        for (k = 0; k < n; k++) {
-          sum += record[j][k];
-        }
-        if (sum == (i+1)) {
-          a = j;
-          trace[a] = 1;
-          break;
+  memset(flag, 0, sizeof(flag));
+  memset(record, 0, sizeof(record));
+  for (i = 1; i < n; i++) {
+    cout << "<" << i << ">";
+    for (j = 1; j <= n; j++) {
+      if (!flag[j]) {
+        for (k = j + 1; k <= n; k++) {
+          if (!flag[k] && !record[j][k] && !record[k][j]) {
+            flag[k] = 1;
+            record[j][k] = record[k][j] = 1;
+            cout << j << "-" << k << " ";
+            break;
+          }
         }
       }
-      // find b
-      for (j = 0; j < n; j++) {
-        if (trace[j] == 0 && record[a][j] == 0) {
-          b = j;
-          trace[b] = 1;
-          break;
-        }
-      }
-      record[a][b] = 1;
-      record[b][a] = 1;
-      cout << a+1 << "-" << b+1 << " ";
     }
     cout << endl;
+    memset(flag, 0, sizeof(flag));
   }
-  system("pause");
+
+  // for (i = 0; i < 16; i++) {
+  //   for (j = 0 ; j < 16; j++) {
+  //     record[i][j] = 0;
+  //   }
+  // }
+  // for (i = 0; i < n; i++) {
+  //   record[i][i] = 1;
+  // }
+  // for (i = 0; i < n-1; i++) {
+  //   for (j = 0; j < n; j++) {
+  //     trace[j] = 0;
+  //   }
+  //   cout << "<" << i+1 << ">";
+  //   for (l = 0; l < n/2; l++) {
+  //     // find a
+  //     for (j = 0; j < n; j++) {
+  //       int sum = 0;
+  //       for (k = 0; k < n; k++) {
+  //         sum += record[j][k];
+  //       }
+  //       if (sum == (i+1)) {
+  //         a = j;
+  //         trace[a] = 1;
+  //         break;
+  //       }
+  //     }
+  //     // find b
+  //     for (j = 0; j < n; j++) {
+  //       if (trace[j] == 0 && record[a][j] == 0) {
+  //         b = j;
+  //         trace[b] = 1;
+  //         break;
+  //       }
+  //     }
+  //     record[a][b] = 1;
+  //     record[b][a] = 1;
+  //     cout << a+1 << "-" << b+1 << " ";
+  //   }
+  //   cout << endl;
+  // }
+  // system("pause");
   return 0;
 }
--- tsinsen/volume1/A1104.cpp
+++ tsinsen/volume1/A1104.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// 多重背包问题
 
 int main(int argc, char const *argv[]) 
 {
-  int a1, a2, a3, a5, a10, a20;
+  int a[6];
+  int b[6] = {1, 2, 3, 5, 10, 20};
   int total = 0;
-  char record[1001];
-  for (int i = 0; i < 1001; i++) {
-    record[i] = 0;
+  int record[1001] = {0};
+  for (int i = 0; i < 6; i++) {
+    cin >> a[i];
   }
-  cin >> a1 >> a2 >> a3 >> a5 >> a10 >> a20;
-  for (int i = 0; i <= a1; i++) {
-    for (int j = 0; j <= a2; j++) {
-      for (int k = 0; k <= a3; k++) {
-        for (int m = 0; m <= a5; m++) {
-          for (int n = 0; n <= a10; n++) {
-            for (int o = 0; o <= a20; o++) {
-              int cr = i*1 + j*2 + k*3 + m*5 + n*10 + o*20;
-              if (record[cr] == 0 && cr > 0) {
-                record[cr] = 1;
-                total++;
-              }
-            }
-          }
+  // for (int i = 0; i <= a1; i++) {
+  //   for (int j = 0; j <= a2; j++) {
+  //     for (int k = 0; k <= a3; k++) {
+  //       for (int m = 0; m <= a5; m++) {
+  //         for (int n = 0; n <= a10; n++) {
+  //           for (int o = 0; o <= a20; o++) {
+  //             int cr = i*1 + j*2 + k*3 + m*5 + n*10 + o*20;
+  //             if (record[cr] == 0 && cr > 0) {
+  //               record[cr] = 1;
+  //               total++;
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  record[0] = 1;
+  for (int i = 0; i < 6; i++) {
+    int k = 1, amount = a[i];
+    if (amount == 0) {
+      continue;
+    }
+    while (k < amount) {
+      int w = k * b[i];
+      for (int j = 1000; j >= w; j--) {
+        if (record[j - w] == 1 && record[j] == 0) {
+          record[j] = 1;
+          total++;
         }
+      }
+      amount -= k;
+      k *= 2;
+    }
+    int w = amount * b[i];
+    for (int j = 1000; j >= w; j--) {
+      if (record[j - w] == 1 && record[j] == 0) {
+        record[j] = 1;
+        total++;
       }
     }
   }
   cout << "Total=" << total << endl;
-  system("pause");
+  // system("pause");
   return 0;
 }
