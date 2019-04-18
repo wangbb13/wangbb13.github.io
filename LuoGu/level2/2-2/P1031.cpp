d5a6218281b7de40a7c499c27591b8eb1286bbf6
wangbb13 <18800160527@163.com>
2019-04-18 21:42:43 +0800

    P1031 done.

--- LuoGu/level2/2-2/P1031.cpp
+++ LuoGu/level2/2-2/P1031.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int pile[100] = {0};
+
 int main() {
-    // 
+    int N, avg = 0, ans = 0;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> pile[i];
+		avg += pile[i];
+	}
+	avg /= N;
+	int cum = 0;
+	for (int i = 0; i < N; ++i) {
+		cum += (pile[i] - avg);
+		if (cum != 0) 
+			ans++;
+	}
+	cout << ans << endl;
     return 0;
 }
-
