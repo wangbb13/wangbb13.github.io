2daf2be3d16fdaaf5422987f48503cacc6b2bdaf
wangbb13 <18800160527@163.com>
2019-05-12 15:26:23 +0800

    P1372 ... (simple)

---
+++ LuoGu/level2/2-21_simple_math/P1372.cpp
@@ -0,0 +1,17 @@
+/**
+ * Description: simple  
+ * Input : 
+ * Output: 
+ * Time/Space Limit: 1s/128MB
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k, r;
+	cin >> n >> k;
+	r = n / k;
+	cout << r << endl;
+    return 0;
+}
+
